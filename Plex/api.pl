e4db8f65158500c9efedf2c940ff1768758111bd
Nate <nate@wolf-paw.net>
2018-09-06 11:09:25 -0400

    Getting ready for all movies

--- Plex/api.pl
+++ Plex/api.pl
@@ -18,7 +18,7 @@ my $baseurl = URI->new($plexpyurl);
 my $apiversion = "api/v2";
 my $apikey = "d1f55a2aec6149199b8f93bdbfbebd92";
 my $days = "7";
-my $selectedlibs = "Newly Downloaded|TV Shows"; #Must have | between Libraries and is case sensitive.
+my $selectedlibs = "Movies"; #Must have | between Libraries and is case sensitive.
 
 ####### END EDIT SECTION #############
 my $now=time;
@@ -65,7 +65,7 @@ foreach my $libs (keys %{$plexfinal{'Libraries'}}) {
 foreach my $section (keys %{$plexfinal{'Libraries'}}) {
 	my $section_id = $plexfinal{'Libraries'}{$section}{'section_id'};
 	my $get_recently_added_url = $baseurl->clone;
-	$get_recently_added_url->query( "apikey=$apikey&count=25&section_id=$section_id&cmd=get_recently_added" );
+	$get_recently_added_url->query( "apikey=$apikey&count=2500&section_id=$section_id&cmd=get_recently_added" );
 	my $get_recently_added = get( $get_recently_added_url );
 	die "Could not get $get_recently_added_url!" unless defined $get_recently_added;
 	my $decoded_get_recently_added = decode_json( $get_recently_added );
@@ -87,7 +87,7 @@ foreach my $section (keys %{$plexfinal{'Libraries'}}) {
 			die "Could not get $get_libraries_url!" unless defined $get_metadata;
 			my $decoded_get_metadata = decode_json( $get_metadata );
 			my %plexmetadata = %$decoded_get_metadata;
-			print Dumper %plexmetadata;
+			#print Dumper %plexmetadata;
 		}
 		else {
 			delete $plexfinal{"Libraries"}{$section_id}{$rating_key};
@@ -98,9 +98,47 @@ foreach my $section (keys %{$plexfinal{'Libraries'}}) {
 	delete $plexfinal{"Libraries"}{$section_id}{"section_id"};
 }
 
-print Dumper %plexfinal;
+print Dumper \%plexfinal;
 
 ################  End Get Recently Added #####################
+
+foreach my $section (keys %{$plexfinal{'Libraries'}}) {
+	my $section_id = $plexfinal{'Libraries'}{$section}{'section_id'};
+	my $get_recently_added_url = $baseurl->clone;
+	$get_recently_added_url->query( "apikey=$apikey&count=2500&section_id=$section_id&cmd=get_recently_added" );
+	my $get_recently_added = get( $get_recently_added_url );
+	die "Could not get $get_recently_added_url!" unless defined $get_recently_added;
+	my $decoded_get_recently_added = decode_json( $get_recently_added );
+	my %plexrecentlyadded = %$decoded_get_recently_added;
+	my $c = "0";
+	foreach my $data (keys @{$plexrecentlyadded{'response'}{'data'}{'recently_added'}}) {
+		my $added_at = $plexrecentlyadded{'response'}{'data'}{'recently_added'}[$c]{'added_at'};
+		my $section_id = $plexrecentlyadded{'response'}{'data'}{'recently_added'}[$c]{'section_id'};
+		my $rating_key = $plexrecentlyadded{'response'}{'data'}{'recently_added'}[$c]{'rating_key'};
+		if ( $added_at <= $now && $added_at >= $past ) {
+			$plexfinal{"Libraries"}{$section_id}{$rating_key}{"rating_key"} = $rating_key;
+			$plexfinal{"Libraries"}{$section_id}{$rating_key} = $plexrecentlyadded{'response'}{'data'}{'recently_added'}[$c];
+			$plexfinal{"Libraries"}{$section_id}{$rating_key}{'section_name'} = $plexfinal{"Libraries"}{$section_id}{"section_name"};
+			$c++;
+			################  Get Metadata ###############################
+			my $get_metadata_url = $baseurl->clone;
+			$get_metadata_url->query( "apikey=$apikey&cmd=get_metadata&rating_key=$rating_key" );
+			my $get_metadata = get( $get_metadata_url );
+			die "Could not get $get_libraries_url!" unless defined $get_metadata;
+			my $decoded_get_metadata = decode_json( $get_metadata );
+			my %plexmetadata = %$decoded_get_metadata;
+			#print Dumper %plexmetadata;
+		}
+		else {
+			delete $plexfinal{"Libraries"}{$section_id}{$rating_key};
+		}
+			
+	}
+	delete $plexfinal{"Libraries"}{$section_id}{"section_name"};
+	delete $plexfinal{"Libraries"}{$section_id}{"section_id"};
+}
+
+print Dumper \%plexfinal;
 
 
 
