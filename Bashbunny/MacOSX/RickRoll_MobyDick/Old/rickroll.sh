202feb4ab61fbf25d5ce8bb8048989007255fad8
Nathan Glaser <nate@wolf-paw.net>
2022-05-02 14:05:22 -0400

    moved BB

--- Bashbunny/MacOSX/RickRoll_MobyDick/Old/rickroll.sh
+++
@@ -1,5 +0,0 @@
-open https://www.youtube.com/watch\?v\=xvFZjo5PgG0
-sleep 10
-#osascript -e "tell application \"Safari\" to quit"
-osascript -e "quit app \"Safari\""
-
--- Bashbunny/MacOSX/RickRoll_MobyDick/reset.sh
+++
@@ -1,1 +0,0 @@
-sudo tccutil reset All
